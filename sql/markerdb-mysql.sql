cf8d3ca9f29e8cd7b75ecff53046b40fb8cd788c
jason <jason@bioperl.org>
2001-06-21 20:47:26 +0000

    named keys and corrected name of map
    svn path=/bioperl-db/trunk/; revision=261

--- sql/markerdb-mysql.sql
+++ sql/markerdb-mysql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE marker (
-	markerid	integer(11) not null AUTO_INCREMENT,
+	markerid	integer(11) not null AUTO_INCREMENT PRIMARY KEY,
 	locus		varchar(64) not null,
 	probe		varchar(64) not null,
 	type		char(5)     not null,
@@ -7,27 +7,25 @@ CREATE TABLE marker (
 	fwdprimer	varchar(128) null,
 	revprimer	varchar(128) null,	
 	length		smallint null,
-	PRIMARY KEY (markerid),
-	UNIQUE KEY (probe),	
-	UNIQUE KEY (fwdprimer, revprimer),
-	KEY (locus),
-	KEY (type)
+	KEY i_locus (locus),
+	KEY i_type (type),
+	UNIQUE KEY i_probe (probe),
+	UNIQUE KEY i_primers (fwdprimer, revprimer)
 );
 
 CREATE TABLE map (
-       mapid integer(10) not null AUTO_INCREMENT,
+       mapid integer(10) not null AUTO_INCREMENT PRIMARY KEY,
        name   varchar(32) not null,
        units  char(5)	 not null,
-       PRIMARY KEY (mapid),
-       UNIQUE KEY (name )       
+       UNIQUE KEY i_name (name )       
 );
 
 CREATE TABLE map_position (
 	markerid	integer(11) not null REFERENCES marker ( markerid ),
 	position	float(8,4)  not null,
 	mapid		integer(10) not null REFERENCES map ( mapid ),
-	PRIMARY KEY (markerid,mapid),
-	KEY (position)
+	PRIMARY KEY pk_marker_map (markerid,mapid),
+	KEY i_position (position)
 );
 
 create table marker_alias (
@@ -50,4 +48,4 @@ INSERT INTO map (name, units) VALUES ( 'gp05sep2000', 'Mb');
 INSERT INTO map (name, units) VALUES ( 'gp12dec2000', 'Mb');
 INSERT INTO map (name, units) VALUES ( 'whitehead', 'cR');
 INSERT INTO map (name, units) VALUES ( 'genemap99gb4', 'cR');
-INSERT INTO map (name, units) VALUES ( 'genemap99g3', 'cR');
+INSERT INTO map (name, units) VALUES ( 'genemap99sg3', 'cR');

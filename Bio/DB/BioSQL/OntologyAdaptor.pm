8ba0636f186e59bf5a1e3a6ad08bbf45f7e17dd7
lapp <lapp@bioperl.org>
2003-03-12 22:38:34 +0000

    Initial revision of Ontology Adaptor.
    svn path=/bioperl-db/trunk/; revision=424

---
+++ Bio/DB/BioSQL/OntologyAdaptor.pm
@@ -0,0 +1,279 @@
+# $Id$
+#
+# BioPerl module for Bio::DB::BioSQL::OntologyAdaptor
+#
+# Cared for by Hilmar Lapp <hlapp at gmx.net>
+#
+
+#
+# (c) Hilmar Lapp, hlapp at gmx.net, 2003.
+# (c) GNF, Genomics Institute of the Novartis Research Foundation, 2003.
+#
+# You may distribute this module under the same terms as perl itself.
+# Refer to the Perl Artistic License (see the license accompanying this
+# software package, or see http://www.perl.com/language/misc/Artistic.html)
+# for the terms under which you may use, modify, and redistribute this module.
+# 
+# THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::DB::BioSQL::OntologyAdaptor - DESCRIPTION of Object
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Ontology DB adaptor 
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this
+and other Bioperl modules. Send your comments and suggestions preferably
+ to one of the Bioperl mailing lists.
+Your participation is much appreciated.
+
+  bioperl-l@bio.perl.org
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.
+ Bug reports can be submitted via email or the web:
+
+  bioperl-bugs@bioperl.org
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Hilmar Lapp
+
+Email hlapp at gmx.net
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::DB::BioSQL::OntologyAdaptor;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble 
+
+use Bio::DB::BioSQL::BasePersistenceAdaptor;
+use Bio::DB::PersistentObjectI;
+use Bio::Ontology::Ontology;
+
+@ISA = qw(Bio::DB::BioSQL::BasePersistenceAdaptor);
+
+
+=head2 new
+
+ Title   : new
+ Usage   :
+ Function: Instantiates the persistence adaptor.
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub new{
+   my ($class,@args) = @_;
+
+   # we want to enable object caching
+   push(@args, "-cache_objects", 1) unless grep { /cache_objects/i; } @args;
+   my $self = $class->SUPER::new(@args);
+
+   return $self;
+}
+
+
+=head2 get_persistent_slots
+
+ Title   : get_persistent_slots
+ Usage   :
+ Function: Get the slots of the object that map to attributes in its respective
+           entity in the datastore.
+
+           Slots should be methods callable without an argument.
+
+ Example :
+ Returns : an array of method names constituting the serializable slots
+ Args    : the object about to be inserted or updated
+
+
+=cut
+
+sub get_persistent_slots{
+    my ($self,@args) = @_;
+
+    return ("name", "definition");
+}
+
+=head2 get_persistent_slot_values
+
+ Title   : get_persistent_slot_values
+ Usage   :
+ Function: Obtain the values for the slots returned by get_persistent_slots(),
+           in exactly that order.
+
+ Example :
+ Returns : A reference to an array of values for the persistent slots of this
+           object. Individual values may be undef.
+ Args    : The object about to be serialized.
+           A reference to an array of foreign key objects if not retrievable 
+           from the object itself.
+
+
+=cut
+
+sub get_persistent_slot_values {
+    my ($self,$obj,$fkobjs) = @_;
+    my @vals = ($obj->name(),
+		$obj->definition()
+		);
+    return \@vals;
+}
+
+=head2 remove_children
+
+ Title   : remove_children
+ Usage   :
+ Function: This method is to cascade deletes in maintained objects.
+
+           We just return TRUE here.
+
+ Example :
+ Returns : TRUE on success and FALSE otherwise
+ Args    : The persistent object that was just removed from the database.
+           Additional (named) parameter, as passed to remove().
+
+
+=cut
+
+sub remove_children{
+    return 1;
+}
+
+=head2 instantiate_from_row
+
+ Title   : instantiate_from_row
+ Usage   :
+ Function: Instantiates the class this object is an adaptor for, and populates
+           it with values from columns of the row.
+
+           This implementation call populate_from_row() to do the real job.
+ Example :
+ Returns : An object, or undef, if the row contains no values
+ Args    : A reference to an array of column values. The first column is the
+           primary key, the other columns are expected to be in the order 
+           returned by get_persistent_slots().
+           Optionally, the object factory to be used for instantiating the
+           proper class. The adaptor must be able to instantiate a default
+           class if this value is undef.
+
+
+=cut
+
+sub instantiate_from_row{
+    my ($self,$row,$fact) = @_;
+    my $obj;
+
+    if($row && @$row) {
+	if($fact) {
+	    $obj = $fact->create_object();
+	} else {
+	    $obj = Bio::Ontology::Ontology->new();
+	}
+	$self->populate_from_row($obj, $row);
+    }
+    return $obj;
+}
+
+=head2 populate_from_row
+
+ Title   : populate_from_row
+ Usage   :
+ Function: Instantiates the class this object is an adaptor for, and populates
+           it with values from columns of the row.
+
+ Example :
+ Returns : An object, or undef, if the row contains no values
+ Args    : The object to be populated.
+           A reference to an array of column values. The first column is the
+           primary key, the other columns are expected to be in the order 
+           returned by get_persistent_slots().
+
+
+=cut
+
+sub populate_from_row{
+    my ($self,$obj,$rows) = @_;
+
+    if(! ref($obj)) {
+	$self->throw("\"$obj\" is not an object. Probably internal error.");
+    }
+    if($rows && @$rows) {
+	$obj->name($rows->[1]) if $rows->[1];
+	$obj->definition($rows->[2]) if $rows->[2];
+	if($obj->isa("Bio::DB::PersistentObjectI")) {
+	    $obj->primary_key($rows->[0]);
+	}
+	return $obj;
+    }
+    return undef;
+}
+
+=head2 get_unique_key_query
+
+ Title   : get_unique_key_query
+ Usage   :
+ Function: Obtain the suitable unique key slots and values as determined by the
+           attribute values of the given object and the additional foreign
+           key objects, in case foreign keys participate in a UK. 
+
+ Example :
+ Returns : A reference to a hash with the names of the object''s slots in the
+           unique key as keys and their values as values.
+ Args    : The object with those attributes set that constitute the chosen
+           unique key (note that the class of the object will be suitable for
+           the adaptor).
+           A reference to an array of foreign key objects if not retrievable 
+           from the object itself.
+
+
+=cut
+
+sub get_unique_key_query{
+    my ($self,$obj,$fkobjs) = @_;
+    my $uk_h = {};
+
+    # UK for ontology is its name
+    if($obj->name()) {
+	$uk_h->{'name'} = $obj->name();
+    }
+    
+    return $uk_h;
+}
+
+=head1 Methods overriden from BasePersistenceAdaptor
+
+=cut
+
+1;

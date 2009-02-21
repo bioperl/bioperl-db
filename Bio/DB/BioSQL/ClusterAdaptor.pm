5c669cbeb203aae58603fc7923920d27b1033ede
maj <maj@bioperl.org>
2009-02-21 22:05:38 +0000

    adding FEEDBACK:Support section to pod
    svn path=/bioperl-db/trunk/; revision=15552

--- Bio/DB/BioSQL/ClusterAdaptor.pm
+++ Bio/DB/BioSQL/ClusterAdaptor.pm
@@ -2,6 +2,8 @@
 #
 # BioPerl module for Bio::DB::BioSQL::ClusterAdaptor
 #
+# Please direct questions and support issues to <bioperl-l@bioperl.org> 
+#
 # Cared for by Hilmar Lapp, hlapp at gmx.net
 #
 # You may distribute this module under the same terms as perl itself
@@ -44,6 +46,17 @@ and other Bioperl modules. Send your comments and suggestions preferably
 Your participation is much appreciated.
 
   bioperl-l@bio.perl.org
+
+=head2 Support 
+ 
+Please direct usage questions or support issues to the mailing list:
+  
+L<bioperl-l@bioperl.org>
+  
+rather than to the module maintainer directly. Many experienced and 
+reponsive experts will be able look at the problem and quickly 
+address it. Please include a thorough description of the problem 
+with code and data examples if at all possible.
 
 =head2 Reporting Bugs
 
